19ee6ceaf4323618f3f67f46c9acbe30ab3e2e95
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2023-11-19 20:37:17 -0500

    added the rest of the time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -2,18 +2,37 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-// added jQuery call
-$(document).ready(function(){
-
-$(function () {
-});
+$(document).ready(function() { 
+$(function(event) {
+  event.preventDefault();
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+
+  // Step 1: Select all save buttons
+var saveButtons = $('.savebtn');
+
+// Step 2: Attach click event listener to save buttons
+saveButtons.on('click', function() {
+  // Step 3: Use `this` to refer to the clicked save button
+  var clickedButton = $(this);
+
+  // Step 4: Navigate to the containing time-block element
+  var timeBlock = clickedButton.closest('.time-block');
+
+  // Step 5: Extract the "hour-x" id
+  var timeBlockId = timeBlock.attr("id");
+
+  // Step 6: Save user input in local storage using the id as the key
+  var userInput = timeBlock.find('.description').val();
+  localStorage.setItem(timeBlockId, userInput);
   
+});
+
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -32,4 +51,8 @@ $(function () {
   $('#currentDay').text(currentDate); 
   var originalDate = dayjs('2023-11-19');
   console.log(originalDate.format('2023-11-19'));
+
+
+
+});
 });
