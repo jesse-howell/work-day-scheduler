89f089d929d18316624cf0a7d64d3df8f84e21c3
Jesse Howell <147555735+jesse-howell@users.noreply.github.com>
2023-11-19 22:22:10 -0500

    added time blocks, added the click listener

--- Develop/script.js
+++ Develop/script.js
@@ -2,9 +2,8 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-$(document).ready(function() { 
-$(function(event) {
-  event.preventDefault();
+$(document).ready(function() {
+$(function() {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -39,6 +38,8 @@ saveButtons.on('click', function() {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -51,6 +52,7 @@ saveButtons.on('click', function() {
   $('#currentDay').text(currentDate); 
   var originalDate = dayjs('2023-11-19');
   console.log(originalDate.format('2023-11-19'));
+
 
 
 
